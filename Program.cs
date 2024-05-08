c2f74265022e568768814e17ed588dafc040cf0b
Gleb Bojarinov <glebbojarinov@yahoo.com>
2024-05-08 12:39:12 +0200

    написали InputConsoleStrings

--- Program.cs
+++ Program.cs
@@ -3,3 +3,15 @@
     Console.Write("Input the number of strings array: ");
     return Convert.ToInt32(Console.ReadLine());
 }
+
+string[] InputConsoleStrings(int InputArray)
+{
+    string[] strings = new string[InputArray];
+    for (int i = 0; i < InputArray; i++)
+    {
+        Console.Write($"Input string {i + 1}: ");
+        strings[i] = Console.ReadLine();
+    }
+    return strings;
+}
+
