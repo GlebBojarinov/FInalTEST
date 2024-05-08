df5795e3c38fdd76745caf5c0642eca4e18b9381
Gleb Bojarinov <glebbojarinov@yahoo.com>
2024-05-08 12:37:36 +0200

    написали InputArraySize

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputArraySize()
+{
+    Console.Write("Input the number of strings array: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
